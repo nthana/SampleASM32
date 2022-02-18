4feea3556606a520582e09680aa234ee59dc1dfa
THANA NITAYARERK <thana_nita@yahoo.com>
2022-02-18 13:34:19 +0700

    Edit comments

--- Main.asm
+++ Main.asm
@@ -9,7 +9,8 @@ MessageBoxA PROTO,
     pCaption:PTR BYTE,
     style:DWORD
 
-; Or use the alternative declarations belows. (also need to edited the calls statements.)
+;   Or use the alternative declarations belows: 
+;   (If used these, also need to edited the call statements' proc names)
 ; extern ExitProcess@4 : PROC
 ; extern MessageBoxA@16 : PROC
 
@@ -20,15 +21,16 @@ myCaption		db "Message Box Testing", 0
 .code
 main PROC
 
-        ; calling MessageBox Win32 API 
-        ; (see: Win32 API MessageBox document)
+        ; see: Win32 API MessageBox
+        ; https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
     push 0                  ; MB_OK 
     push offset myCaption   ; caption's start address
     push offset myText      ; text's start address
     push 0                  ; hwnd: Owner window's handle
     call MessageBoxA        ; Or call MessageBoxA@16
 	
-
+        ; see: Win32 API ExitProcess
+        ; https://docs.microsoft.com/en-us/windows/win32/api/processthreadsapi/nf-processthreadsapi-exitprocess
 	push 12345678           ; exit code
 	call ExitProcess        ; Or call ExitProcess@4
 main ENDP
